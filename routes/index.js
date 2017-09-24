73662895773eb63504b47d22c9b365c74b0ce66c
CptnChoas <jan-mue@gmx.de>
2017-09-24 17:13:51 +0200

    added developer access token

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,45 @@ var https = require('https');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', {title:'asd', tests:['a','b','c']});
+    var accessToken = '7be0c816e737376d7aad968e29f96880e51446e8be9efbc0730bf489cc0f69d8';
+    options = {
+        hostname: 'api.producthunt.com',
+        port: 443,
+        path: '/v1/posts',
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + accessToken,
+            //'Authorization': 'Bearer' + new Buffer(key + ":" + secret, "utf8").toString("base64"),
+            'Host': 'api.producthunt.com'
+        }
+    };
+
+    var get = https.request(options, function (data) {
+        var output = '';
+        data.setEncoding('utf8');
+
+        data.on('data', function (chunk) {
+            output += chunk;
+        });
+
+        data.on('end', function() {
+            var obj = JSON.parse(output);
+            if(obj.hasOwnProperty('error')){
+                res.render('error', {message:'Error', error:{status:obj.error, stack:obj.error_description}})
+            }
+            else{
+                res.render('index', {posts:obj.posts});
+            }
+        });
+    });
+
+    get.on('error', function (err) {
+        res.render('error');
+    });
+
+    get.end();
 });
 
 /* GET home page. */
